c5d992e5e07d9c341a28fd673ca010e5b3c945b7
XiaomA <14351441+wxma@user.noreply.gitee.com>
2025-08-24 17:27:55 +0800

    2.2 项目初始化：整合依赖 - 其他依赖(按需引入，如AOP)

--- src/main/java/com/maxiaowei/yupicturebackend/Application.java
+++ src/main/java/com/maxiaowei/yupicturebackend/Application.java
@@ -3,12 +3,14 @@ package com.maxiaowei.yupicturebackend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * 启动类
  */
 @SpringBootApplication
 @MapperScan("com.maxiaowei.yupicturebackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class Application {
 
     public static void main(String[] args) {
