f721bfe067dc2eaa7918a5190fea43185a550da2
XiaomA <14351441+wxma@user.noreply.gitee.com>
2025-08-24 20:53:56 +0800

    3.3 用户模块：用户注册功能 - 注册服务开发

--- src/main/java/com/maxiaowei/yupicturebackend/service/UserService.java
+++ src/main/java/com/maxiaowei/yupicturebackend/service/UserService.java
@@ -9,5 +9,21 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2025-08-24 20:20:15
 */
 public interface UserService extends IService<User> {
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
 
+    /**
+     * 获取加密后的密码
+     *
+     * @param userPassword
+     * @return
+     */
+    String getEncryptPassword(String userPassword);
 }
--- src/main/java/com/maxiaowei/yupicturebackend/service/impl/UserServiceImpl.java
+++ src/main/java/com/maxiaowei/yupicturebackend/service/impl/UserServiceImpl.java
@@ -1,19 +1,78 @@
 package com.maxiaowei.yupicturebackend.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.maxiaowei.yupicturebackend.exception.BusinessException;
+import com.maxiaowei.yupicturebackend.exception.ErrorCode;
+import com.maxiaowei.yupicturebackend.model.enums.UserRoleEnum;
 import com.maxiaowei.yupicturebackend.model.pojo.User;
 import com.maxiaowei.yupicturebackend.service.UserService;
 import com.maxiaowei.yupicturebackend.mapper.UserMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 /**
-* @author Administrator
-* @description 针对表【user(用户)】的数据库操作Service实现
-* @createDate 2025-08-24 20:20:15
-*/
+ * @author Administrator
+ * @description 针对表【user(用户)】的数据库操作Service实现
+ * @createDate 2025-08-24 20:20:15
+ */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
-    implements UserService{
+        implements UserService {
+
+    @Override
+    public long userRegister(String userAccount, String userPassword, String checkPassword) {
+        // 1. 校验
+        checkValidBeforeRegister(userAccount, userPassword, checkPassword);
+
+        // 2. 校验是否重复
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount", userAccount);
+        Long count = this.baseMapper.selectCount(queryWrapper);
+        if (count > 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "账号重复");
+        }
+
+        // 3. 加密
+        String encryptPassword = getEncryptPassword(userPassword);
+
+        // 4. 插入数据
+        User user = new User();
+        user.setUserAccount(userAccount);
+        user.setUserPassword(encryptPassword);
+        user.setUserName("无名氏");
+        user.setUserRole(UserRoleEnum.USER.getValue());
+        boolean isSave = this.save(user);
+        if (isSave) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "注册失败， 数据库异常");
+        }
+        return user.getId();
+    }
+
+    @Override
+    public String getEncryptPassword(String userPassword) {
+        // 加盐值，混淆密码
+        final String SALT = "xiaowei";
+        return DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+    }
+
+    private void checkValidBeforeRegister(String userAccount, String userPassword, String checkPassword) {
+        // 1. 校验格式是否合法
+        if (StrUtil.hasBlank(userAccount, userPassword, checkPassword)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "参数为空");
+        }
+        if (userAccount.length() < 4) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户账号过短");
+        }
+        if (userPassword.length() < 8 || checkPassword.length() < 8) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户密码过短");
+        }
+        if (!userPassword.equals(checkPassword)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "两次输入的密码不一致");
+        }
+    }
+
 
 }
 
