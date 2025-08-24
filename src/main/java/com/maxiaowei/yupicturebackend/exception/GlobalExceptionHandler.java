9747a50dea5062303eee1a485f674b88ac56faed
XiaomA <14351441+wxma@user.noreply.gitee.com>
2025-08-24 17:37:38 +0800

    2.3 项目初始化：通用基础代码 - 全局异常处理器

---
+++ src/main/java/com/maxiaowei/yupicturebackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.maxiaowei.yupicturebackend.exception;
+
+import com.maxiaowei.yupicturebackend.common.BaseResponse;
+import com.maxiaowei.yupicturebackend.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误请联系管理员");
+    }
+}
