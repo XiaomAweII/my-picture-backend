5ca33921be2bb9bc0bc7b9863d0df39d376565c7
XiaomA <14351441+wxma@user.noreply.gitee.com>
2025-08-26 20:48:50 +0800

    4.1 图片模块：后端操作对象存储 - 文件下载 测试

--- src/main/java/com/maxiaowei/yupicturebackend/controller/FileController.java
+++ src/main/java/com/maxiaowei/yupicturebackend/controller/FileController.java
@@ -7,15 +7,17 @@ import com.maxiaowei.yupicturebackend.common.constant.UserConstant;
 import com.maxiaowei.yupicturebackend.common.exception.BusinessException;
 import com.maxiaowei.yupicturebackend.common.exception.ErrorCode;
 import com.maxiaowei.yupicturebackend.manager.CosManager;
+import com.qcloud.cos.model.COSObject;
+import com.qcloud.cos.model.COSObjectInputStream;
+import com.qcloud.cos.utils.IOUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -54,6 +56,37 @@ public class FileController {
                 if (!delete) {
                     log.error("file delete error, filepath = " + filePath);
                 }
+            }
+        }
+    }
+
+    /**
+     * 测试文件下载
+     *
+     * @param filePath
+     * @param response
+     */
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    @GetMapping("/test/download")
+    public void testDownloadFile(String filePath, HttpServletResponse response) throws IOException {
+        COSObjectInputStream cosObjectInput = null;
+        try {
+            COSObject cosObject = cosManager.getObject(filePath);
+            cosObjectInput = cosObject.getObjectContent();
+            // 处理下载到的流
+            byte[] bytes = IOUtils.toByteArray(cosObjectInput);
+            // 设置响应头
+            response.setContentType("application/octet-stream;charset=UTF-8");
+            response.setHeader("Content-Disposition", "attachment; filename=" + filePath);
+            // 写入响应
+            response.getOutputStream().write(bytes);
+            response.getOutputStream().flush();
+        } catch (Exception e) {
+            log.error("file download error, filePath = {}", filePath, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "下载失败");
+        } finally {
+            if (null != cosObjectInput) {
+                cosObjectInput.close();
             }
         }
     }
--- src/main/java/com/maxiaowei/yupicturebackend/manager/CosManager.java
+++ src/main/java/com/maxiaowei/yupicturebackend/manager/CosManager.java
@@ -2,6 +2,8 @@ package com.maxiaowei.yupicturebackend.manager;
 
 import com.maxiaowei.yupicturebackend.config.CosClientConfig;
 import com.qcloud.cos.COSClient;
+import com.qcloud.cos.model.COSObject;
+import com.qcloud.cos.model.GetObjectRequest;
 import com.qcloud.cos.model.PutObjectRequest;
 import com.qcloud.cos.model.PutObjectResult;
 import org.springframework.stereotype.Component;
@@ -23,12 +25,23 @@ public class CosManager {
     /**
      * 上传对象
      *
-     * @param key 唯一键
+     * @param key  唯一键
      * @param file 文件
      * @return
      */
     public PutObjectResult putObject(String key, File file) {
         PutObjectRequest putObjectResult = new PutObjectRequest(cosClientConfig.getBucket(), key, file);
         return cosClient.putObject(putObjectResult);
+    }
+
+    /**
+     * 下载对象
+     *
+     * @param key 唯一键
+     * @return
+     */
+    public COSObject getObject(String key) {
+        GetObjectRequest getObjectRequest = new GetObjectRequest(cosClientConfig.getBucket(), key);
+        return cosClient.getObject(getObjectRequest);
     }
 }
