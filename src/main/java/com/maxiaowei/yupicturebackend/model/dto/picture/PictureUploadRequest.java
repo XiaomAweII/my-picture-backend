aa7d0fcd71b6d978d2d0f74e3d1b683bba7783ec
XiaomA <14351441+wxma@user.noreply.gitee.com>
2025-08-26 21:04:27 +0800

    4.2 图片模块：图片上传 - 数据模型（请求和返回前端视图）

---
+++ src/main/java/com/maxiaowei/yupicturebackend/model/dto/picture/PictureUploadRequest.java
@@ -0,0 +1,15 @@
+package com.maxiaowei.yupicturebackend.model.dto.picture;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class PictureUploadRequest implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 图片 id（用于修改）
+     */
+    private Long id;
+}
