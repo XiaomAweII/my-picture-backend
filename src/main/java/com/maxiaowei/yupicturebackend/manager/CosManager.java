e88dab9cff7a81681c17388de130e183fce7d58c
XiaomA <14351441+wxma@user.noreply.gitee.com>
2025-08-27 00:19:32 +0800

    4.2 图片模块：图片上传 - 拓展使用流方式上传文件

--- src/main/java/com/maxiaowei/yupicturebackend/manager/CosManager.java
+++ src/main/java/com/maxiaowei/yupicturebackend/manager/CosManager.java
@@ -2,19 +2,21 @@ package com.maxiaowei.yupicturebackend.manager;
 
 import com.maxiaowei.yupicturebackend.config.CosClientConfig;
 import com.qcloud.cos.COSClient;
-import com.qcloud.cos.model.COSObject;
-import com.qcloud.cos.model.GetObjectRequest;
-import com.qcloud.cos.model.PutObjectRequest;
-import com.qcloud.cos.model.PutObjectResult;
+import com.qcloud.cos.model.*;
 import com.qcloud.cos.model.ciModel.persistence.PicOperations;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * 通用的对象存储操作
  */
+@Slf4j
 @Component
 public class CosManager {
     @Resource
@@ -62,5 +64,36 @@ public class CosManager {
         // 构造处理参数
         putObjectRequest.setPicOperations(picOperations);
         return cosClient.putObject(putObjectRequest);
+    }
+
+
+    /**
+     * 上传文件(还没有测试过)
+     * 不对文件进行额外处理，通过流的方式将请求中的文件上传到COS，提高性能
+     *
+     * @param multipartFile 文件
+     * @param key           唯一键
+     * @return
+     */
+    public String uploadToCOS(MultipartFile multipartFile, String key) throws IOException {
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            // 元信息配置
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(multipartFile.getSize());
+            metadata.setContentType(multipartFile.getContentType());
+
+            // 创建上传请求
+            PutObjectRequest putObjectRequest = new PutObjectRequest(cosClientConfig.getBucket(), key, inputStream, metadata);
+
+            // 上传文件
+            cosClient.putObject(putObjectRequest);
+
+            // 生成访问链接
+            return "https://" + cosClientConfig.getBucket() + ".cos." +
+                    "." + cosClient.getClientConfig().getRegion().getRegionName() + ".myqcloud.com/" + key;
+        } catch (Exception e) {
+            log.error("CosManager uploadToCOS error", e);
+        }
+        return null;
     }
 }
