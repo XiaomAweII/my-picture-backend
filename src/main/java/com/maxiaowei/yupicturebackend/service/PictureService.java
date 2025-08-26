4a9e44477cffc2facb8fc797ed6607572b500634
XiaomA <14351441+wxma@user.noreply.gitee.com>
2025-08-26 23:51:46 +0800

    4.2 图片模块：图片上传 - 图片上传服务开发

--- src/main/java/com/maxiaowei/yupicturebackend/service/PictureService.java
+++ src/main/java/com/maxiaowei/yupicturebackend/service/PictureService.java
@@ -1,7 +1,11 @@
 package com.maxiaowei.yupicturebackend.service;
 
+import com.maxiaowei.yupicturebackend.model.dto.picture.PictureUploadRequest;
 import com.maxiaowei.yupicturebackend.model.pojo.Picture;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.maxiaowei.yupicturebackend.model.pojo.User;
+import com.maxiaowei.yupicturebackend.model.vo.PictureVO;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
 * @author Administrator
@@ -9,5 +13,15 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2025-08-26 20:50:58
 */
 public interface PictureService extends IService<Picture> {
-
+    /**
+     * 上传图片
+     *
+     * @param multipartFile
+     * @param pictureUploadRequest
+     * @param loginUser
+     * @return
+     */
+    PictureVO uploadPicture(MultipartFile multipartFile,
+                            PictureUploadRequest pictureUploadRequest,
+                            User loginUser);
 }
--- src/main/java/com/maxiaowei/yupicturebackend/service/impl/PictureServiceImpl.java
+++ src/main/java/com/maxiaowei/yupicturebackend/service/impl/PictureServiceImpl.java
@@ -1,20 +1,71 @@
 package com.maxiaowei.yupicturebackend.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.maxiaowei.yupicturebackend.common.exception.ErrorCode;
+import com.maxiaowei.yupicturebackend.common.exception.ThrowUtils;
+import com.maxiaowei.yupicturebackend.manager.FileManager;
+import com.maxiaowei.yupicturebackend.model.dto.file.UploadPictureResult;
+import com.maxiaowei.yupicturebackend.model.dto.picture.PictureUploadRequest;
 import com.maxiaowei.yupicturebackend.model.pojo.Picture;
+import com.maxiaowei.yupicturebackend.model.pojo.User;
+import com.maxiaowei.yupicturebackend.model.vo.PictureVO;
 import com.maxiaowei.yupicturebackend.service.PictureService;
 import com.maxiaowei.yupicturebackend.mapper.PictureMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.util.Date;
 
 /**
-* @author Administrator
-* @description 针对表【picture(图片)】的数据库操作Service实现
-* @createDate 2025-08-26 20:50:58
-*/
+ * @author Administrator
+ * @description 针对表【picture(图片)】的数据库操作Service实现
+ * @createDate 2025-08-26 20:50:58
+ */
 @Service
 public class PictureServiceImpl extends ServiceImpl<PictureMapper, Picture>
-    implements PictureService{
+        implements PictureService {
 
+    @Resource
+    private FileManager fileManager;
+
+    @Override
+    public PictureVO uploadPicture(MultipartFile multipartFile, PictureUploadRequest pictureUploadRequest, User loginUser) {
+        ThrowUtils.throwIf(null == loginUser, ErrorCode.NOT_LOGIN_ERROR);
+        // 用于判断是新增还是更新图片
+        Long pictureId = null;
+        if (pictureUploadRequest != null) {
+            pictureId = pictureUploadRequest.getId();
+        }
+        // 如果是更新图片， 需要校验图片是否存在
+        if (pictureId != null) {
+            boolean exists = this.lambdaQuery().eq(Picture::getId, pictureId).exists();
+            ThrowUtils.throwIf(!exists, ErrorCode.NOT_FOUND_ERROR, "图片不存在");
+        }
+        // 上传图片，得到信息
+        // 按照用户 id 划分目录
+        String uploadPathPrefix = String.format("public/%s", loginUser.getId());
+        UploadPictureResult uploadPictureResult = fileManager.uploadPicture(multipartFile, uploadPathPrefix);
+        // 构造要入库的图片信息
+        Picture picture = new Picture();
+        picture.setUrl(uploadPictureResult.getUrl());
+        picture.setName(uploadPictureResult.getPicName());
+        picture.setPicSize(uploadPictureResult.getPicSize());
+        picture.setPicWidth(uploadPictureResult.getPicWidth());
+        picture.setPicHeight(uploadPictureResult.getPicHeight());
+        picture.setPicScale(uploadPictureResult.getPicScale());
+        picture.setPicFormat(uploadPictureResult.getPicFormat());
+        picture.setUserId(loginUser.getId());
+        // 如果 pictureId 不为空，表示更新，否则是新增
+        if (pictureId != null) {
+            // 如果是更新，需要补充id和编辑时间
+            picture.setId(pictureId);
+            picture.setEditTime(new Date());
+        }
+        boolean result = this.saveOrUpdate(picture);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR, "图片上传失败");
+        return PictureVO.objToVo(picture);
+    }
 }
 
 
